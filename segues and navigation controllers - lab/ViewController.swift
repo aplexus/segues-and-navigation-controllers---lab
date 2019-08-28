441c72eda3d3939a61c30d302f7edf7c519ee728
Aleksey Popov <aplexus11@yandex.ru>
2019-08-28 11:14:14 +0300

    Initial Commit

---
+++ segues and navigation controllers - lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segues and navigation controllers - lab
+//
+//  Created by Aleksey Popov on 28/08/2019.
+//  Copyright © 2019 Aleksey Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
